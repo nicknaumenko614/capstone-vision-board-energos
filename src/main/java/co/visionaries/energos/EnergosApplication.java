5d48b196a3908b3399f6a499cf566373b475d68e
Ben Williams <DeanPBR@gmail.com>
2020-07-27 13:01:35 -0400

    Initial commit

---
+++ src/main/java/co/visionaries/energos/EnergosApplication.java
@@ -0,0 +1,13 @@
+package co.visionaries.energos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnergosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnergosApplication.class, args);
+    }
+
+}
---
+++ src/test/java/co/visionaries/energos/EnergosApplicationTests.java
@@ -0,0 +1,13 @@
+package co.visionaries.energos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnergosApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
