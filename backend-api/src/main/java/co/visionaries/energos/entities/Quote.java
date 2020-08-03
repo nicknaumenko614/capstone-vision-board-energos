8e48054d402dd31a1bca2d36b16974dd22623005
Derek Mosher <derekmosher21@gmail.com>
2020-08-03 14:02:42 -0400

    added Quote pojo

---
+++ backend-api/src/main/java/co/visionaries/energos/entities/Quote.java
@@ -0,0 +1,120 @@
+package co.visionaries.energos.entities;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+@Entity
+public class Quote {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String quoteName;
+    private int quoteHeight;
+    private int quoteWidth;
+    private String quoteLink;
+    private boolean quoteHasBorder;
+    private int quoteBorderRadius;
+    private int quoteRotate;
+    private boolean quoteIsFlipped;
+
+    @ManyToOne
+    private VisionBoard visionBoard;
+
+    public Quote() {
+    }
+
+    public Quote(long id, String quoteName, int quoteHeight, int quoteWidth, String quoteLink, boolean quoteHasBorder, int quoteBorderRadius, int quoteRotate, boolean quoteIsFlipped, VisionBoard visionBoard) {
+        this.id = id;
+        this.quoteName = quoteName;
+        this.quoteHeight = quoteHeight;
+        this.quoteWidth = quoteWidth;
+        this.quoteLink = quoteLink;
+        this.quoteHasBorder = quoteHasBorder;
+        this.quoteBorderRadius = quoteBorderRadius;
+        this.quoteRotate = quoteRotate;
+        this.quoteIsFlipped = quoteIsFlipped;
+        this.visionBoard = visionBoard;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getQuoteName() {
+        return quoteName;
+    }
+
+    public void setQuoteName(String quoteName) {
+        this.quoteName = quoteName;
+    }
+
+    public int getQuoteHeight() {
+        return quoteHeight;
+    }
+
+    public void setQuoteHeight(int quoteHeight) {
+        this.quoteHeight = quoteHeight;
+    }
+
+    public int getQuoteWidth() {
+        return quoteWidth;
+    }
+
+    public void setQuoteWidth(int quoteWidth) {
+        this.quoteWidth = quoteWidth;
+    }
+
+    public String getQuoteLink() {
+        return quoteLink;
+    }
+
+    public void setQuoteLink(String quoteLink) {
+        this.quoteLink = quoteLink;
+    }
+
+    public boolean isQuoteHasBorder() {
+        return quoteHasBorder;
+    }
+
+    public void setQuoteHasBorder(boolean quoteHasBorder) {
+        this.quoteHasBorder = quoteHasBorder;
+    }
+
+    public int getQuoteBorderRadius() {
+        return quoteBorderRadius;
+    }
+
+    public void setQuoteBorderRadius(int quoteBorderRadius) {
+        this.quoteBorderRadius = quoteBorderRadius;
+    }
+
+    public int getQuoteRotate() {
+        return quoteRotate;
+    }
+
+    public void setQuoteRotate(int quoteRotate) {
+        this.quoteRotate = quoteRotate;
+    }
+
+    public boolean isQuoteIsFlipped() {
+        return quoteIsFlipped;
+    }
+
+    public void setQuoteIsFlipped(boolean quoteIsFlipped) {
+        this.quoteIsFlipped = quoteIsFlipped;
+    }
+
+    public VisionBoard getVisionBoard() {
+        return visionBoard;
+    }
+
+    public void setVisionBoard(VisionBoard visionBoard) {
+        this.visionBoard = visionBoard;
+    }
+}
