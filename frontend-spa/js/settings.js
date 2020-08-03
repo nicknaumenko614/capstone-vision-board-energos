d4f0ae52388e5a4f7e8b1d4f559d6efe1d3660f9
Nick Naumenko <carswithcharacter614@gmail.com>
2020-08-03 13:19:15 -0400

    resize dark mode logo

--- frontend-spa/js/settings.js
+++ frontend-spa/js/settings.js
@@ -1,15 +1,19 @@
 const body = document.body;
 const logoImage = document.querySelector(".logo-image");
 const darkModeToggleSwitch = document.querySelector(".dark-theme-checkbox");
-
+const icons = document.querySelectorAll(".icon-div i");
 
 export function toggleDarkMode() {
   body.classList.toggle("dark");
-  if (darkModeToggleSwitch.checked){
-      logoImage.src = "./images/logo-dark.png";
-      logoImage.style.marginTop = "20px";
+  if (darkModeToggleSwitch.checked) {
+    logoImage.src = "./images/logo-dark.png";
+    logoImage.style.marginTop = "20px";
+    logoImage.style.transform = "scale(1)";
   } else {
-      logoImage.src = "./images/logo.png";
-      logoImage.style.marginTop = "";
+    logoImage.src = "./images/logo.png";
+    logoImage.style.marginTop = "";
+    logoImage.style.transform = "scale(1.2)";
   }
+
+  icons.forEach((icon) => icon.classList.toggle("dark"));
 }
