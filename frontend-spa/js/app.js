1dc40c44ece71529c865e7520e049b9905799b8d
20Claudia20 <leiaofalderran@gmail.com>
2020-08-04 15:02:31 -0400

    created templates js file

--- frontend-spa/js/app.js
+++ frontend-spa/js/app.js
@@ -1,7 +1,8 @@
 import { addSettingsEventListeners } from "./settings.js";
 import { addSideBarEventListeners } from "./sidebar.js";
+import { addEventListenersToTemplates } from "./templates.js";
 
 addSettingsEventListeners();
 addSideBarEventListeners();
-
+addEventListenersToTemplates();
 
---
+++ frontend-spa/js/templates.js
@@ -0,0 +1,10 @@
+const templates = document.querySelectorAll(".templates-container img");
+
+export function addEventListenersToTemplates() {
+    for (let i = 0; i < templates.length; i++) {
+        templates[i].addEventListener ("click", ()=> {
+
+        });
+    }
+
+}
