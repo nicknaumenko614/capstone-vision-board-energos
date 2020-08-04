9c7dbf42b18941021b02a48de1e01fb270dfba47
jrobson1065 <j.robson1065@gmail.com>
2020-08-04 10:13:11 -0400

    removed hover text from sidebar icons when sidebar content is visible

--- frontend-spa/js/siderbar.js
+++ frontend-spa/js/siderbar.js
@@ -8,11 +8,16 @@ export function addSideBarEventListeners() {
   for (let i = 0; i < icons.length; i++) {
     icons[i].addEventListener("click", () => {
       sidebarContent.style.display = "block";
+      icons.forEach(icon => icon.classList.remove("icon-text"));
+      icons.forEach(icon => icon.classList.remove("active"));
+      icons[i].classList.add("active");
       setTimeout(() => {
         sidebarContent.style.width = "16vw";
       }, 50);
       setTimeout(() => {
-        contentContainers.forEach((container) => (container.style.display = "none"));
+        contentContainers.forEach(
+          (container) => (container.style.display = "none")
+        );
         contentContainers[i].style.display = "flex";
       }, 300);
     });
@@ -21,6 +26,11 @@ export function addSideBarEventListeners() {
   setTimeout(() => {
     main.addEventListener("mouseover", () => {
       sidebarContent.style.width = "0";
+
+      for (let i = 0; i < icons.length; i++) {
+        icons[i].classList.add("icon-text");
+        icons[i].classList.remove("active");
+      }
 
       setTimeout(() => {
         sidebarContent.style.display = "none";
