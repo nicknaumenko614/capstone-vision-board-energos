d2953cd20a90e223dc137555fbb11c8b36420803
Kendyl Hull <hullkendyl@gmail.com>
2020-08-04 13:42:06 -0400

    refactored eventlisteners

--- frontend-spa/js/sidebar.js
+++ frontend-spa/js/sidebar.js
@@ -4,43 +4,28 @@ const main = document.querySelector("main");
 const icons = document.querySelectorAll(".icon-div i");
 const contentContainers = document.querySelectorAll(".sidebar-content > div");
 const addTextInput = document.querySelector(".add-text");
-const inputContainerDiv = document.querySelector(".text-container .input-container");
-const backgroundColorPickerInput = document.querySelector(".wallpapers-container .color-picker");
-const backgroundAddImageInput = document.querySelector(".wallpapers-container .add-image");
-const backgroundImageInputContainer = document.querySelector(".input-container");
+const inputContainerDiv = document.querySelector(
+  ".text-container .input-container"
+);
+const backgroundColorPickerInput = document.querySelector(
+  ".wallpapers-container .color-picker"
+);
+const backgroundAddImageInput = document.querySelector(
+  ".wallpapers-container .add-image"
+);
+const backgroundImageInputContainer = document.querySelector(
+  ".input-container"
+);
 
 export function addSideBarEventListeners() {
   for (let i = 0; i < icons.length; i++) {
     icons[i].addEventListener("click", () => {
-      sidebarContent.style.display = "block";
-      icons.forEach((icon) => icon.classList.remove("icon-text"));
-      icons.forEach((icon) => icon.classList.remove("active"));
-      icons[i].classList.add("active");
-      setTimeout(() => {
-        sidebarContent.style.width = "16vw";
-      }, 50);
-      setTimeout(() => {
-        contentContainers.forEach(
-          (container) => (container.style.display = "none")
-        );
-        contentContainers[i].style.display = "flex";
-      }, 300);
+      addHoverEffectsForIcons();
     });
   }
 
-  setTimeout(() => {
-    main.addEventListener("mouseover", () => {
-      sidebarContent.style.width = "0";
-
-      for (let i = 0; i < icons.length; i++) {
-        icons[i].classList.add("icon-text");
-        icons[i].classList.remove("active");
-      }
-
-      setTimeout(() => {
-        sidebarContent.style.display = "none";
-      }, 300);
-    });
+  main.addEventListener("mouseover", () => {
+    hideSidebarContent();
   });
 
   addTextInput.addEventListener("change", () => {
@@ -57,6 +42,35 @@ export function addSideBarEventListeners() {
   });
 }
 
+function addHoverEffectsForIcons() {
+  sidebarContent.style.display = "block";
+  icons.forEach((icon) => icon.classList.remove("icon-text"));
+  icons.forEach((icon) => icon.classList.remove("active"));
+  icons[i].classList.add("active");
+  setTimeout(() => {
+    sidebarContent.style.width = "16vw";
+  }, 50);
+  setTimeout(() => {
+    contentContainers.forEach(
+      (container) => (container.style.display = "none")
+    );
+    contentContainers[i].style.display = "flex";
+  }, 300);
+}
+
+function hideSidebarContent() {
+  sidebarContent.style.width = "0";
+
+  for (let i = 0; i < icons.length; i++) {
+    icons[i].classList.add("icon-text");
+    icons[i].classList.remove("active");
+  }
+
+  setTimeout(() => {
+    sidebarContent.style.display = "none";
+  }, 300);
+}
+
 let textNumber = 0;
 function createNewTextElement() {
   let textDiv = document.createElement("div");
@@ -69,7 +83,10 @@ function createNewTextElement() {
   textH2.contentEditable = "true";
   textH2.innerText = textFromInput;
   textDiv.appendChild(textH2);
-  inputContainerDiv.parentNode.insertBefore(textDiv, inputContainerDiv.nextSibling);
+  inputContainerDiv.parentNode.insertBefore(
+    textDiv,
+    inputContainerDiv.nextSibling
+  );
   addTextInput.value = "";
 }
 
@@ -86,6 +103,13 @@ function createNewBackgroundImage() {
   }
   let img = new Image();
   img.src = url;
-  backgroundImageInputContainer.parentNode.insertBefore(img, backgroundImageInputContainer.nextSibling);
+  backgroundImageInputContainer.parentNode.insertBefore(
+    img,
+    backgroundImageInputContainer.nextSibling
+  );
   backgroundAddImageInput.value = "";
+}
+
+function addEventListenerToWallpaperImages() {
+  
 }
