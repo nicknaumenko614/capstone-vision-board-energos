34f0364580f900c8aa9289f3a45ae51e9dc5b15f
Jheath122576 <65102308+Jheath122576@users.noreply.github.com>
2020-08-05 14:14:13 -0400

    Added dragNdrop feature to JS

--- frontend-spa/js/app.js
+++ frontend-spa/js/app.js
@@ -1,8 +1,10 @@
 import { addSettingsEventListeners } from "./settings.js";
 import { addSideBarEventListeners } from "./sidebar.js";
 import { addEventListenersToTemplates } from "./templates.js";
+import { addDragAndDropEventListeners } from "./drag-and-drop.js";
 
 addSettingsEventListeners();
 addSideBarEventListeners();
 addEventListenersToTemplates();
+addDragAndDropEventListeners();
 
--- frontend-spa/js/drag-and-drop.js
+++ frontend-spa/js/drag-and-drop.js
@@ -1,27 +1,29 @@
 const boxes = document.querySelectorAll(".box");
+const images = document.querySelectorAll("img");
 
-boxes.forEach((box) => {
-    box.addEventListener("drop",()=> {
-        drop(event, box);
+export function addDragAndDropEventListeners() {
+  boxes.forEach((box) => {
+    box.addEventListener("drop", () => {
+      drop(event, box);
     });
 
-    box.addEventListener("dragOver",()=> {
-        allowDrop(event);
-    })
-});
+    box.addEventListener("dragOver", () => {
+      allowDrop(event);
+    });
+  });
+}
 
 function allowDrop(e) {
-    e.preventDefault();
+  e.preventDefault();
 }
 
 function drag(e) {
-    e.dataTransfer.setData("text", e.target.id);
+  e.dataTransfer.setData("text", e.target.id);
 }
 
 function drop(e, box) {
-    e.preventDefault();
-    let data = e.dataTransfer.getData("text");
-    let el = document.getElementById(data);
-    box.append(el);
+  e.preventDefault();
+  let data = e.dataTransfer.getData("text");
+  let el = document.getElementById(data);
+  box.append(el);
 }
-
