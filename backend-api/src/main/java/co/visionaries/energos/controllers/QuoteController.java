4f538a6b21cf72af1e5e2cb471e1bed70ee08985
20Claudia20 <leiaofalderran@gmail.com>
2020-08-07 11:42:17 -0400

    created first get mapping

--- backend-api/src/main/java/co/visionaries/energos/controllers/QuoteController.java
+++ backend-api/src/main/java/co/visionaries/energos/controllers/QuoteController.java
@@ -1,8 +1,12 @@
 package co.visionaries.energos.controllers;
 
+import co.visionaries.energos.entities.Quote;
 import co.visionaries.energos.storage.QuoteStorage;
 import co.visionaries.energos.storage.VisionBoardStorage;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
 
 @RestController
 public class QuoteController {
@@ -13,6 +17,9 @@ public class QuoteController {
         this.quoteStorage = quoteStorage;
         this.visionBoardStorage = visionBoardStorage;
     }
+
+    @GetMapping("/api/quotes")
+    public Collection<Quote> getAllQuotes(){return quoteStorage.getAllQuotes();}
 
     
 
