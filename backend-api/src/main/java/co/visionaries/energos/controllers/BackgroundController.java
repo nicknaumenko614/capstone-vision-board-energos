3567c65919dfee186e503441e786f34746d0b320
Kendyl Hull <hullkendyl@gmail.com>
2020-08-07 12:24:45 -0400

    created GetMapping for backgrounds

--- backend-api/src/main/java/co/visionaries/energos/controllers/BackgroundController.java
+++ backend-api/src/main/java/co/visionaries/energos/controllers/BackgroundController.java
@@ -1,4 +1,25 @@
 package co.visionaries.energos.controllers;
 
+import co.visionaries.energos.entities.Background;
+import co.visionaries.energos.storage.BackgroundStorage;
+import co.visionaries.energos.storage.VisionBoardStorage;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Collection;
+
+@RestController
 public class BackgroundController {
+    BackgroundStorage backgroundStorage;
+    VisionBoardStorage visionBoardStorage;
+
+
+public BackgroundController(BackgroundStorage backgroundStorage, VisionBoardStorage visionBoardStorage) {
+    this.backgroundStorage = backgroundStorage;
+    this.visionBoardStorage = visionBoardStorage;
+}
+
+@GetMapping("/api/backgrounds")
+public Collection<Background> getAllBackgrounds() { return backgroundStorage.getAllBackgrounds();}
+
 }
