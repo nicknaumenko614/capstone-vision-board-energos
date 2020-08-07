c0fac7e1723f9d645ee96ce60ce8caf42dbcc01a
Jheath122576 <65102308+Jheath122576@users.noreply.github.com>
2020-08-07 12:04:43 -0400

    created post mapping for quotes controller

--- backend-api/src/main/java/co/visionaries/energos/controllers/QuoteController.java
+++ backend-api/src/main/java/co/visionaries/energos/controllers/QuoteController.java
@@ -1,6 +1,7 @@
 package co.visionaries.energos.controllers;
 
 import co.visionaries.energos.entities.Quote;
+import co.visionaries.energos.entities.VisionBoard;
 import co.visionaries.energos.storage.QuoteStorage;
 import co.visionaries.energos.storage.VisionBoardStorage;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -24,10 +25,17 @@ public class QuoteController {
     public Collection<Quote> getAllQuotes(){return quoteStorage.getAllQuotes();}
 
     @GetMapping("/api/quotes/{quoteId}")
-    public Quote getQuoteById(@PathVariable long quoteId){ return quoteStorage.retrieveQuoteById(quoteId);}
+    public Quote getQuoteById(@PathVariable long quoteId) { return quoteStorage.retrieveQuoteById(quoteId);}
 
     @DeleteMapping("/api/quotes/{quoteId}/delete")
     public void deleteQuote(@PathVariable long quoteId) {
         quoteStorage.deleteQuote(quoteId);
+    }
+
+    @PostMapping("/api/visionboard/{visionBoardId}/addquote")
+    public void addQuoteToVisionBoard(@PathVariable long visionBoardId, @RequestBody Quote quote) {
+        VisionBoard visionBoard = visionBoardStorage.retrieveVisionBoardbyId(visionBoardId);
+        quote.setVisionBoard(visionBoard);
+        quoteStorage.saveQuote(quote);
     }
 }
