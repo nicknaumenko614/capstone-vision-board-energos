836f9530dd46f1e655630cfe4d4897f6482f312b
20Claudia20 <leiaofalderran@gmail.com>
2020-08-07 11:38:08 -0400

    begun quotes controller

--- backend-api/src/main/java/co/visionaries/energos/controllers/QuoteController.java
+++ backend-api/src/main/java/co/visionaries/energos/controllers/QuoteController.java
@@ -1,9 +1,20 @@
 package co.visionaries.energos.controllers;
 
+import co.visionaries.energos.storage.QuoteStorage;
+import co.visionaries.energos.storage.VisionBoardStorage;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class QuoteController {
+    QuoteStorage quoteStorage;
+    VisionBoardStorage visionBoardStorage;
+
+    public QuoteController(QuoteStorage quoteStorage, VisionBoardStorage visionBoardStorage){
+        this.quoteStorage = quoteStorage;
+        this.visionBoardStorage = visionBoardStorage;
+    }
+
     
+
 
 }
