18dca68fcb420db665ce9334c07a0b53b48769e6
20Claudia20 <leiaofalderran@gmail.com>
2020-08-09 13:32:55 -0400

    completed welcome page creation

--- frontend-spa/js/components/createWelcomePage.js
+++ frontend-spa/js/components/createWelcomePage.js
@@ -1,36 +1,32 @@
-export const createWelcomePage = (visionBoards)=> {
-    const body = document.body;
+export const createWelcomePage = (visionBoards) => {
+  const container = document.createElement("div");
 
-    const img = new Image();
-    img.classList.add("logo-image");
-    img.src = "./images/logo.png";
-    img.alt = "Energos Logo";
+  const img = new Image();
+  img.classList.add("logo-image");
+  img.src = "./images/logo.png";
+  img.alt = "Energos Logo";
 
-    const header = document.createElement("header");
-    header.innerHTML = `
+  const header = document.createElement("header");
+  header.innerHTML = `
     <div class="welcome-design">
         <h1>Welcome!</h1>
         <h2>Choose Your Vision Board</h2>
       </div>
     `;
 
-    const main = document.createElement("main");
-    const welcomeDesignBoard = document.createElement("div");
-    welcomeDesignBoard.classList.add("welcome-design-board");
-    visionBoards.forEach(visionBoard => {
-        const welcomeBox = document.createElement("div");
-        welcomeBox.classList.add("welcome-box");
+  const main = document.createElement("main");
+  const welcomeDesignBoard = document.createElement("div");
+  welcomeDesignBoard.classList.add("welcome-design-board");
+  visionBoards.forEach((visionBoard) => {
+    const welcomeBox = document.createElement("div");
+    welcomeBox.classList.add("welcome-box");
+    welcomeDesignBoard.appendChild(welcomeBox);
+    main.appendChild(welcomeDesignBoard);
+  });
 
-    });
-}
+    container.append(img, header, main);  
+
+    return container;
+};
 
 
-
-
-{/* <body>
-    <img class="logo-image" src="./images/logo.png" alt="Energos Logo" />
-    <header>
-    </header>
-    <main> 
-    </main>
-  </body> */}
