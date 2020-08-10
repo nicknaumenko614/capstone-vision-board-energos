706aa830542d555b68a530ac0b59cdc09a68874f
20Claudia20 <leiaofalderran@gmail.com>
2020-08-10 11:17:14 -0400

    working on vision board tab and left side

--- frontend-spa/js/components/createVisionBoard.js
+++ frontend-spa/js/components/createVisionBoard.js
@@ -1,14 +1,20 @@
 import {clearElementChildren} from "../app.js";
+import { renderDesignBoard } from "./renderDesignBoards.js";
 
 export function createVisionBoard(visionBoard){
     const containerBody = document.querySelector(".container-body");
 
     clearElementChildren(containerBody);
-    
+
     const image = new Image();
     image.classList.add("logo-image");
     image.src = "./images/logo.png";
     image.alt = "Energós Logo";
-
+    
+    containerBody.appendChild(image);
     containerBody.appendChild(createLeftSideBar());
+
+    const main = document.createElement("main");
+    main.appendChild(renderDesignBoard(`${visionBoard.gridTemplateName}`))
 }
+
