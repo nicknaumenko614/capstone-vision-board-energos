a1be69cbabb8eef22c097e21fb8004c915b18184
20Claudia20 <leiaofalderran@gmail.com>
2020-08-10 21:57:49 -0400

    adde id to boxes

--- frontend-spa/js/components/renderDesignBoards.js
+++ frontend-spa/js/components/renderDesignBoards.js
@@ -13,19 +13,19 @@ function renderDesignBoard1() {
     designBoard.classList.add("design-board");
     designBoard.innerHTML = `
     <div class="row">
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
+    <div class="box" id="box1"></div>
+    <div class="box" id="box2"></div>
+    <div class="box" id="box3"></div>
   </div>
   <div class="row">
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
+    <div class="box" id="box4"></div>
+    <div class="box" id="box5"></div>
+    <div class="box" id="box6"></div>
   </div>
   <div class="row">
-    <div class="box"></div>
-    <div class="box"></div>
-    <div class="box"></div>
+    <div class="box" id="box7"></div>
+    <div class="box" id="box8"></div>
+    <div class="box" id="box9"></div>
   </div>
     `
     return designBoard;
@@ -37,20 +37,20 @@ function renderDesignBoard2() {
   designBoard.innerHTML = `
   <div class="column-left">
           <div class="row-single">
-            <div class="box"></div>
+            <div class="box" id="box10"></div>
           </div>
           <div class="row-double">
-            <div class="box"></div>
-            <div class="box"></div>
+            <div class="box" id="box11"></div>
+            <div class="box" id="box12"></div>
           </div>
         </div>
         <div class="column-right">
           <div class="row-double">
-            <div class="box"></div>
-            <div class="box"></div>
+            <div class="box" id="box13"></div>
+            <div class="box" id="box14"></div>
           </div>
           <div class="row-single">
-            <div class="box"></div>
+            <div class="box" id="box15"></div>
           </div>
         </div>
   `
@@ -62,12 +62,12 @@ function renderDesignBoard3() {
   designBoard.classList.add("design-board3");
   designBoard.innerHTML = `
   <div class="column-left">
-          <div class="box"></div>
+          <div class="box" id="box16"></div>
         </div>
         <div class="column-right">
-          <div class="box"></div>
-          <div class="box"></div>
-          <div class="box"></div>
+          <div class="box" id="box17"></div>
+          <div class="box" id="box18"></div>
+          <div class="box" id="box19"></div>
         </div>
   `
   return designBoard;
@@ -78,31 +78,31 @@ function renderDesignBoard4() {
   designBoard.classList.add("design-board4");
   designBoard.innerHTML = `
   <div class="column-end">
-          <div class="box"></div>
-          <div class="box"></div>
-          <div class="box"></div>
-          <div class="box"></div>
+          <div class="box" id="box20"></div>
+          <div class="box" id="box21"></div>
+          <div class="box" id="box22"></div>
+          <div class="box" id="box23"></div>
         </div>
         <div class="column-middle">
           <div class="row-end">
-            <div class="box"></div>
-            <div class="box"></div>
-            <div class="box"></div>
+            <div class="box" id="box24"></div>
+            <div class="box" id="box25"></div>
+            <div class="box" id="box26"></div>
           </div>
           <div class="row-middle">
-            <div class="box"></div>
+            <div class="box" id="box27"></div>
           </div>
           <div class="row-end">
-            <div class="box"></div>
-            <div class="box"></div>
-            <div class="box"></div>
+            <div class="box" id="box28"></div>
+            <div class="box" id="box29"></div>
+            <div class="box" id="box30"></div>
           </div>
         </div>
         <div class="column-end">
-          <div class="box"></div>
-          <div class="box"></div>
-          <div class="box"></div>
-          <div class="box"></div>
+          <div class="box" id="box31"></div>
+          <div class="box" id="box32"></div>
+          <div class="box" id="box33"></div>
+          <div class="box" id="box34"></div>
         </div>
   `
   return designBoard;
@@ -114,24 +114,24 @@ function renderDesignBoard5() {
   designBoard.innerHTML = `
   <div class="column-left">
           <div class="row-top">
-            <div class="box"></div>
-            <div class="box"></div>
+            <div class="box" id="box35"></div>
+            <div class="box" id="box36"></div>
           </div>
           <div class="row-bottom">
-            <div class="box"></div>
-            <div class="box"></div>
+            <div class="box" id="box37"></div>
+            <div class="box" id="box38"></div>
           </div>
         </div>
         <div class="column-right">
           <div class="row-top">
-            <div class="box"></div>
+            <div class="box" id="box39"></div>
           </div>
           <div class="row-middle">
-            <div class="box"></div>
+            <div class="box" id="box40"></div>
           </div>
           <div class="row-bottom">
-            <div class="box"></div>
-            <div class="box"></div>
+            <div class="box" id="box41"></div>
+            <div class="box" id="box42"></div>
           </div>
         </div>
   `
@@ -143,17 +143,17 @@ function renderDesignBoard6() {
   designBoard.classList.add("design-board6");
   designBoard.innerHTML = `
   <div class="column-end">
-          <div class="box"></div>
-          <div class="box"></div>
-          <div class="box"></div>
+          <div class="box" id="box43"></div>
+          <div class="box" id="box44"></div>
+          <div class="box" id="box45"></div>
         </div>
         <div class="column-middle">
-          <div class="box"></div>
+          <div class="box" id="box46"></div>
         </div>
         <div class="column-end">
-          <div class="box"></div>
-          <div class="box"></div>
-          <div class="box"></div>
+          <div class="box" id="box47"></div>
+          <div class="box" id="box48"></div>
+          <div class="box" id="box49"></div>
         </div>
   `
   return designBoard;
