6c10bff7ec10028e563b65253fe3f9c367f441f6
jrobson1065 <j.robson1065@gmail.com>
2020-08-11 20:23:01 -0400

    added function to render visionboard border based on last saved options

--- frontend-spa/js/components/createVisionBoard.js
+++ frontend-spa/js/components/createVisionBoard.js
@@ -41,15 +41,16 @@ function addAllEventListeners() {
   addSettingsEventListeners();
 }
 
-function renderPreSavedOptions(visionboard){
+function renderPreSavedOptions(visionboard) {
   darkThemeOption(visionboard);
   showGridOption(visionboard);
   gridSpacingOption(visionboard);
   borderRadiusOption(visionboard);
+  borderOption(visionboard);
 }
 
-function darkThemeOption(visionboard){
-  if (`${visionboard.themeDark}` == "true"){ 
+function darkThemeOption(visionboard) {
+  if (`${visionboard.themeDark}` == "true") {
     const body = document.body;
     const icons = document.querySelectorAll(".icon-div i");
     const logoImage = document.querySelector(".logo-image");
@@ -60,11 +61,11 @@ function darkThemeOption(visionboard){
     icons.forEach((icon) => icon.classList.add("dark"));
     const darkModeToggleSwitch = document.querySelector(".dark-theme-checkbox");
     darkModeToggleSwitch.checked = true;
-  } 
+  }
 }
 
-function showGridOption(visionboard){
-  if (`${visionboard.hasGrid}` == "false"){
+function showGridOption(visionboard) {
+  if (`${visionboard.hasGrid}` == "false") {
     const showGridToggleSwitch = document.querySelector(".grid-checkbox");
     const gridBoxes = document.querySelectorAll(".box");
     gridBoxes.forEach((box) => (box.style.backgroundColor = "transparent"));
@@ -72,20 +73,33 @@ function showGridOption(visionboard){
   }
 }
 
-function gridSpacingOption(visionboard){
+function gridSpacingOption(visionboard) {
   const spacingSlider = document.querySelector("#spacing-slider");
   const elementsWithGridGap = document.querySelectorAll("main div:not(.box)");
   const gap = `${visionboard.gridSpacing}`;
-  elementsWithGridGap.forEach(
-    (element) => (element.style.gridGap = gap)
-  );
+  elementsWithGridGap.forEach((element) => (element.style.gridGap = gap));
   spacingSlider.value = parseInt(gap, 10);
 }
 
-function  borderRadiusOption(visionboard){
+function borderRadiusOption(visionboard) {
   const borderRadiusSlider = document.querySelector("#border-radius-slider");
   const gridBoxes = document.querySelectorAll(".box");
   const radius = `${visionboard.gridBorderRadius}`;
   gridBoxes.forEach((box) => (box.style.borderRadius = radius));
   borderRadiusSlider.value = parseInt(radius, 10);
+}
+
+function borderOption(visionboard) {
+  const borderColorPicker = document.querySelector(".border-color");
+  const borderStyleSelector = document.querySelector(".border-style");
+  const main = document.querySelector("main");
+  const borderThicknessSlider = document.querySelector(
+    "#border-thickness-slider"
+  );
+  const color = `${visionboard.gridBorderColor}`;
+  const thickness = `${visionboard.gridBorderThickness}`;
+  const style = `${visionboard.gridBorderStyle}`;
+  console.log(color, thickness, style);
+
+  main.style.border = thickness + " " + style + " " + color;
 }
